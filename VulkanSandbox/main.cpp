e3945d965574ce0c55429d57e51450bcc6334cc8
One290 <lenya.dorovskoy@mail.ru>
2025-05-05 14:11:09 +0800

    added json logging
    Signed-off-by: One290 <lenya.dorovskoy@mail.ru>

--- VulkanSandbox/main.cpp
+++ VulkanSandbox/main.cpp
@@ -2,14 +2,39 @@
 #include "spdlog/async.h"
 #include "spdlog/sinks/rotating_file_sink.h"
 
-std::shared_ptr<spdlog::logger> CreateLogger()
-{
-    auto ret = spdlog::rotating_logger_mt<spdlog::async_factory>("async_file_logger",
-        "async_log.txt", 1'073'741'824, 2);
+std::shared_ptr<spdlog::logger> CreateLogger() {
+    auto ret = spdlog::rotating_logger_mt<spdlog::async_factory>("logger", "log.json",
+        1'073'741'824, 2);
+    const auto spdlog_tp = spdlog::thread_pool();
 
-    ret->set_pattern("[%P %t] %+");
+    ret->set_pattern(R"({"log":[)");
+    ret->info("");
+    ret->flush();
+    while (spdlog_tp->queue_size() != 0)
+        std::this_thread::yield();
+
+    ret->set_pattern(R"({"time":"%Y-%m-%d %H:%M:%S.%e","level":"%l","process":%P,"thread":%t,"message":%v},)");
+    ret->info(R"("Logger created.")");
+    ret->flush();
+    while (spdlog_tp->queue_size() != 0)
+        std::this_thread::yield();
 
     return ret;
+}
+
+void DestroyLogger(const std::shared_ptr<spdlog::logger> &logger) {
+    const auto spdlog_tp = spdlog::thread_pool();
+
+    logger->set_pattern(R"({"time":"%Y-%m-%d %H:%M:%S.%e","level":"%l","process":%P,"thread":%t,"message":%v})");
+    logger->info(R"("Logger destroyed.")");
+    logger->flush();
+    while (spdlog_tp->queue_size() != 0)
+        std::this_thread::yield();
+
+    logger->set_pattern("]}");
+    logger->info("");
+
+    spdlog::shutdown();
 }
 
 void Configuring()
@@ -19,8 +44,8 @@ void Configuring()
 
 void Body(const std::shared_ptr<spdlog::logger> &logger)
 {
-    for (int i = 1; i <= 8000; i++) {
-        logger->info("Zxc = {}", i);
+    for (auto i = 0; i < 10; i++) {
+        logger->info(R"("Zxc = {}")", i);
     }
 }
 
@@ -30,14 +55,13 @@ int main()
 
     std::thread bodyThread(Body, logger);
 
-    for (int i = 1; i <= 8000; i++) {
-        logger->info("Cxz = {}", i);
-
+    for (auto i = 0; i < 10; i++) {
+        logger->info(R"("Cxz = {}")", i);
     }
 
     bodyThread.join();
 
-    spdlog::shutdown();
+    DestroyLogger(logger);
 
     return 0;
 }
